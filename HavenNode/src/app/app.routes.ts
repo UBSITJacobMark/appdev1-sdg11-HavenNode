60d71d3204d9aab99d502c7e52f41f1419be189e
UBSITJacobMark <20244156@s.ubaguio.edu>
2026-04-15 12:54:32 +0800

    Attempting to Fix THE Dashboard

--- HavenNode/src/app/app.routes.ts
+++ HavenNode/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from '../pages/home/home';
+import { DashboardComponent } from '../pages/dashboard/dashboard';
+import { NodeDetail } from '../pages/node-detail/node-detail';
+import { NotFound } from '../pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'dashboard', component: DashboardComponent},
+    { path: 'dashboard/:id', component: NodeDetail },
+    { path: '**', component: NotFound }
+  ];
