058d6403609d562a6b029e2ecd2c8b6953ebeb6b
UBSITSabelaSVinChrysler <20243818@s.ubaguio.edu>
2026-04-22 12:11:10 +0800

    Made the Home page as default,Instead of the Map, then added the html and css of the Home page, and fixed the routing of the navbar

--- HavenNode/src/app/app.routes.ts
+++ HavenNode/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { About } from '../pages/about/about';
+import { Home } from '../pages/home/home';
 
 export const routes: Routes = [
-    {path: 'about', component: About}
+    {path: 'about', component: About},
+    { path: '', component: Home } //Default Home Page Not Map
 ];
