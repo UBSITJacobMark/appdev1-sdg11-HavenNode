c8bd1b1e3e215fec44e462610ecdb218c4da3338
UBSITEscuadraSK <20243641@.ubaguio.edu>
2026-04-22 13:01:51 +0800

    Made the Sign in page. Continueing to do Register page

--- HavenNode/src/app/app.routes.ts
+++ HavenNode/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { DashboardComponent } from '../pages/dashboard/dashboard';
 import { NodeDetailComponent } from '../pages/node-detail/node-detail'; // ✅ Match class name
 import { NotFoundComponent } from '../pages/not-found/not-found'; // ✅ Match class name
 import { About } from '../pages/about/about';
+import { Login } from '../pages/login/login'; // add this
 
 export const routes: Routes = [
     {path: 'about', component: About},
 
     { path: '', component: HomeComponent },
+    { path: 'login', component: Login }, 
     { path: 'dashboard', component: DashboardComponent },
     { path: 'dashboard/:id', component: NodeDetailComponent }, // ✅ Required URL parameter [cite: 49]
     { path: '**', component: NotFoundComponent } // ✅ Required wildcard route [cite: 50]
