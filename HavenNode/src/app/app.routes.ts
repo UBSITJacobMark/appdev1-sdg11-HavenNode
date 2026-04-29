bf30e43354c0f078b728466ae0ba161af4246829
UBSITJacobMark <20244156@s.ubaguio.edu>
2026-04-29 13:47:44 +0800

    FIRE FIGHTING

--- HavenNode/src/app/app.routes.ts
+++ HavenNode/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../pages/home/home'; // ✅ Changed 'Home' to 'HomeComponent'
-import { DashboardComponent } from '../pages/dashboard/dashboard';
-import { NotFoundComponent } from '../pages/not-found/not-found'; // ✅ Match class name
+import { DashboardComponent } from '../pages/dashboard/dashboard.component';
+import { NotFoundComponent } from '../pages/not-found/not-found.component'; // ✅ Match class name
 import { About } from '../pages/about/about';
 import { Login } from '../pages/login/login'; 
 import { Register } from '../pages/register/register';
@@ -9,5 +9,25 @@ import { Contact } from '../pages/contact/contact';
 
 
 export const routes: Routes = [
-    {path: 'about', component: About}
+    {path: 'about', component: About},
+    {
+        path: '',
+        loadComponent: () => import('../pages/home/home').then(m => m.HomeComponent),
+        title: 'HavenNode | Benguet DRRM'
+    },
+    {
+        path: 'dashboard',
+        loadComponent: () => import('../pages/dashboard/dashboard.component').then(m => m.DashboardComponent),
+        title: 'HavenNode | Command Center'
+    },
+    {
+        path: 'about',
+        loadComponent: () => import('../pages/about/about').then(m => m.About),
+        title: 'HavenNode | About SDG 11'
+    },  
+    {
+    path: '**', 
+    loadComponent: () => import('../pages/not-found/not-found.component').then(m => m.NotFoundComponent),
+    title: 'HavenNode | Page Not Found'
+    }
 ];
